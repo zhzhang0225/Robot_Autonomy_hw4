316c2c30a20985559927a54f19879416a7e8b9f6
aumj <aum.jadhav@gmail.com>
2017-04-01 23:02:02 -0400

    Some bigfixes (still not perfect)

--- code/AStarPlanner.py
+++ code/AStarPlanner.py
@@ -1,5 +1,6 @@
 import numpy as np
 from Queue import PriorityQueue
+from SimpleEnvironment import Action
 
 class AStarPlanner(object):
 
@@ -28,7 +29,7 @@ class AStarPlanner(object):
 
         start_id = self.planning_env.discrete_env.ConfigurationToNodeId(start_config)
         goal_id = self.planning_env.discrete_env.ConfigurationToNodeId(goal_config)
-        print "Start state = ", start_config, ", int_state=", init_state
+        print "Start state = ", start_config, ", init_state=", init_state
         print "Start ID = ", start_id
 
         # plan = np.empty((0, start_config.size()))
@@ -46,9 +47,10 @@ class AStarPlanner(object):
 
         # Add initial neighbors to the frontier
         successors, succ_acts = self.planning_env.GetSuccessors(start_id)
+        # print successors
         came_from_actions.update(succ_acts)
         for next_node in successors:
-            print next_node
+            # print next_node
             plan_cost[next_node] = plan_cost[start_id] + 1
             priority = plan_cost[next_node] + self.planning_env.ComputeHeuristicCost(next_node, goal_id)
             frontier.put((priority, next_node))
@@ -57,32 +59,30 @@ class AStarPlanner(object):
         while frontier:
             (cur_cost, cur_node) = frontier.get()
             states_visited.append(cur_node)
-            # if self.visualize and hasattr(self.planning_env, 'InitializePlot'):
-            #     self.planning_env.PlotEdge(self.planning_env.discrete_env.NodeIdToConfiguration(came_from[cur_node]), self.planning_env.discrete_env.NodeIdToConfiguration(cur_node))
 
             if cur_node == goal_id:
                 goal_found = True
                 print "Goal Found!"
                 break
 
-            successors = self.planning_env.GetSuccessors(cur_node)
-            successors, succ_acts = self.planning_env.GetSuccessors(start_id)
+            successors, succ_acts = self.planning_env.GetSuccessors(cur_node)
+            # print successors
             came_from_actions.update(succ_acts)
             for next_node in successors:
-                print next_node
+                # print next_node
                 new_cost = plan_cost[cur_node] + 1
-                # self.planning_env.PlotEdge(self.planning_env.discrete_env.NodeIdToConfiguration(cur_node), self.planning_env.discrete_env.NodeIdToConfiguration(next_node))
-                if next_node not in plan_cost  or  new_cost < plan_cost[next_node]:
+                if (next_node not in plan_cost)  or  (new_cost < plan_cost[next_node]):
                     plan_cost[next_node] = new_cost
                     priority = new_cost + self.planning_env.ComputeHeuristicCost(next_node, goal_id)
                     frontier.put((priority, next_node))
                     came_from[next_node] = cur_node
 
+        if not goal_found:
+            print "Path not found"
+
         while goal_found  and  came_from[cur_node] is not None:
-            # parent = came_from[cur_node]
-            # plan = np.append([self.planning_env.discrete_env.NodeIdToConfiguration(cur_node)], plan, axis=0)
             parent, action = came_from_actions[cur_node]
-            plan = np.append([action], plan, axis=0)
+            plan = [action] + plan
             cur_node = parent
 
         print 'NUM OF EXPANDED NODES: ' + repr(len(states_visited))
